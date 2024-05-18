45f4a07a53eb0d860c42a5ea8ec3d97a3a6e6a08
sadek1999 <sadekali1530@gmail.com>
2024-05-18 11:56:29 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+
+import { Server } from 'http';
+import app from './app';
+const PORT =5000;
+
+let server:Server;
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+  })
